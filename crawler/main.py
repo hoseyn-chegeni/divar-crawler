88878e71530df801e601900eb9ad4b9b3a286977
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-06-20 10:59:57 +0330

    write first api and test the connection

--- crawler/main.py
+++ crawler/main.py
@@ -1,3 +1,9 @@
 from fastapi import FastAPI
+import requests
 
 app = FastAPI()
+
+@app.get("/crawls")
+def read_crawls():
+    response = requests.get("http://job_service:8001/jobs")
+    return {"message": "This is the crawler service", "jobs_response": response.json()}
--- job/main.py
+++ job/main.py
@@ -1,3 +1,7 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+
+@app.get("/jobs")
+def read_jobs():
+    return {"message": "This is the job service"}
