11c9c7d2252482d7bd444e1fb44371e292a79347
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-06-26 12:46:48 +0330

    add read crawled data with id

--- crawler/main.py
+++ crawler/main.py
@@ -32,12 +32,12 @@ def read_crawled_data(skip: int = 0, limit: int = 100, db: Session = Depends(get
     return crawled_data
 
 
-# @app.get("/users/{user_id}", response_model=schemas.User)
-# def read_user(user_id: int, db: Session = Depends(get_db)):
-#     db_user = crud.get_user(db, user_id=user_id)
-#     if db_user is None:
-#         raise HTTPException(status_code=404, detail="user not found")
-#     return db_user
+@app.get("/crawled_data/{crawled_data_id}/", response_model=schemas.CrawledData)
+def read_crawled_data(crawled_data_id: int, db: Session = Depends(get_db)):
+    db_crawled_data= crud.get_crawled_data(db, crawled_data_id=crawled_data_id)
+    if db_crawled_data is None:
+        raise HTTPException(status_code=404, detail="data not found")
+    return db_crawled_data
 
 
 @app.get("/crawl-title/")
