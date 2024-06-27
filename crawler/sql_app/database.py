ad5cc100eb7bbb126cbe74ea33df31d418d45d21
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-06-27 21:19:20 +0330

    migrate db to postgresql for both service

--- crawler/sql_app/database.py
+++ crawler/sql_app/database.py
@@ -2,11 +2,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+SQLALCHEMY_DATABASE_URL = "postgresql://crawler_user:admin@postgres_crawler:5432/crawler_db"
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
