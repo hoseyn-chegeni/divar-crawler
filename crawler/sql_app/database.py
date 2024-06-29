77e199947c2be6a48ccf9de3b6305ec9574e2ae5
hoseyn <87471826+hoseyn-chegeni@users.noreply.github.com>
2024-06-29 01:29:14 -0700

    roleback to sqlite3

--- crawler/sql_app/database.py
+++ crawler/sql_app/database.py
@@ -1,16 +1,11 @@
-from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
 
-SQLALCHEMY_DATABASE_URL = "postgresql://crawler_user:admin@postgres_crawler:5432/crawler_db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
--- job/sql_app/database.py
+++ job/sql_app/database.py
@@ -1,16 +1,11 @@
-from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
 
-SQLALCHEMY_DATABASE_URL = "postgresql://job_user:admin@postgres_job:5432/job_db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
